7445b6baa4f09ca374e54fdebc3fba54c561b029
Stusigh <stuartjohnpage@live.com>
2021-02-02 08:36:23 -0600

    changed comments

--- src/dataStructureHomeworkPartTwo/Main.java
+++ src/dataStructureHomeworkPartTwo/Main.java
@@ -2,16 +2,16 @@ package dataStructureHomeworkPartTwo;
 
 public class Main {
     public static void main(String[] args) {
-        //Part One
-        System.out.println("This is the answer to question one: ");
-        int[] myArray = {5,754,34,753,36};
-        int answerOne = FirstQuestion.sumTheArray(myArray);
+        //First Part
+        System.out.println("Question one: ");
+        int[] givenArray = {5,754,34,753,36};
+        int answerOne = FirstQuestion.sumTheArray(givenArray);
         System.out.println(answerOne);
-        //Part Two
+        //Second Part
         System.out.println("This is the output of question two (the index of the biggest number in the given array): ");
         SecondQuestion.exampleCodeGiven();
-        //Part Three
-        System.out.println("This is the array returned in question three: ");
+        //Third Part
+        System.out.println("Array returned in question three: ");
         int[] answerThree = ThirdQuestion.toPower(4,2);
         for(int number: answerThree) {
             System.out.println(number);
